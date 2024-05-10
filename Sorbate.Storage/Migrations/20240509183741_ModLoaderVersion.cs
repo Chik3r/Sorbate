929c80700d31ae399faa0b7fb03d70d500f79ec9
Chik3r <31808958+Chik3r@users.noreply.github.com>
2024-05-10 20:02:31 +0100

    migrations

---
+++ Sorbate.Storage/Migrations/20240509183741_ModLoaderVersion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sorbate.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModLoaderVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ModLoaderVersion",
+                table: "Files",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModLoaderVersion",
+                table: "Files");
+        }
+    }
+}
