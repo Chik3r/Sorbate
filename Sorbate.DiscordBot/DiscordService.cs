9443c0ed3566d2f35e39a862eca7d2fb276b54b1
Chik3r <31808958+Chik3r@users.noreply.github.com>
2024-06-16 18:19:53 +0100

    new project for main logic

---
+++ Sorbate.DiscordBot/DiscordService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Sorbate.DiscordBot;
+
+public class DiscordService : BackgroundService {
+    protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Sorbate/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sorbate.DiscordBot;
+using Sorbate.Storage.Models;
+
+namespace Sorbate;
+
+class Program {
+    private static async Task Main(string[] args) {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+        builder.Services.AddLogging();
+        builder.Services.AddHostedService<DiscordService>();
+        builder.Services.AddDbContext<StorageContext>();
+
+        using IHost host = builder.Build();
+        await host.RunAsync();
+    }
+}
