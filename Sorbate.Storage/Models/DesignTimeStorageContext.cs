44395e358b18f4d4442b9230a1465f000ef35ba2
Chik3r <31808958+Chik3r@users.noreply.github.com>
2024-06-23 20:59:05 +0100

    DesignTimeStorageContext

---
+++ Sorbate.Storage/Models/DesignTimeStorageContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Sorbate.Storage.Models;
+
+public class DesignTimeStorageContext : IDesignTimeDbContextFactory<StorageContext> {
+    public StorageContext CreateDbContext(string[] args) {
+        DbContextOptionsBuilder<StorageContext> optionsBuilder = new();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres;");
+        
+        return new StorageContext(optionsBuilder.Options);
+
+    }
+}
