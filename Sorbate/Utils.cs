4dda82eb492d3067fbfe4256f1d288ea6e8d4149
Chik3r <31808958+Chik3r@users.noreply.github.com>
2026-08-13 14:15:58 +0200

    fix the list.trygetvalue

--- Sorbate/Utils.cs
+++ Sorbate/Utils.cs
@@ -3,8 +3,15 @@
 public static class Utils {
     extension<T>(IReadOnlyList<(string path, T value)> list) {
         public bool TryGetValue(string target, out T? val) {
-            val = list.FirstOrDefault(x => x.path == target).value;
-            return val is not null;
+            var tmp = list.FirstOrDefault(x => x.path == target, ("null.null", default)!);
+
+            if (tmp.path == "null.null") {
+                val = default;
+                return false;
+            }
+
+            val = tmp.value;
+            return true;
         }
     }
 }
