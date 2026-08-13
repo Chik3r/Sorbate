99528eade3b32e23b11276b65e418884727109db
Chik3r <31808958+Chik3r@users.noreply.github.com>
2026-08-13 13:53:54 +0200

    FNB now uses lists instead of dicts

---
+++ Sorbate/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace Sorbate;
+
+public static class Utils {
+    extension<T>(IReadOnlyList<(string path, T value)> list) {
+        public bool TryGetValue(string target, out T? val) {
+            val = list.FirstOrDefault(x => x.path == target).value;
+            return val is not null;
+        }
+    }
+}
